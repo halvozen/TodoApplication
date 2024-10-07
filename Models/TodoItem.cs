216ec1ceaa39d367fe820f007e7819adad1afc4d
Tande-Halvorsen <tande.halvorsen@gmail.com>
2024-10-07 22:10:15 +0200

    I am adding an incomplete version of the Todo application to have version control as I continue to develop the code.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace TodoApplication.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; } = false;
+    }
+}
+
+
